c992556c793625a1830bfa7746a66cfaa10102a5
SmarikaGhimire <smarikaghimire@gmail.com>
2023-12-24 18:21:49 +0545

    initial commit

---
+++ src/contacts.js
@@ -0,0 +1,21 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    img: "https://imgs.search.brave.com/lSYiNXsAlWxE_n3DSk9m0O6F8PplCmTT3X0XO5ajSnA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzViLzY1/L2MyLzViNjVjMjli/NTgzZDRkMjEzOWNj/MmExOGQ1YWZiMWNh/LmpwZw",
+    tel: "9846326027",
+    email: "beyoncewei@gmail.com",
+  },
+  {
+    name: "Taylor Swift",
+    img: "https://hips.hearstapps.com/hmg-prod/images/taylor-swift-6527b5f05095e.jpg?crop=1xw:0.5xh;center,top&resize=1200:*",
+    tel: "9846034567",
+    email: "Taylorswift@gmail.com",
+  },
+  {
+    name: "Zayn Malik",
+    img: "https://m.media-amazon.com/images/M/MV5BMjZhNTk0ZjQtNDI0NS00NDA5LThmZjctMzFlNmMwZjhmYmY2XkEyXkFqcGdeQXVyMTMzOTMyOTM4._V1_FMjpg_UX1000_.jpg",
+    tel: "003 455 678",
+    email: "zayn@gmail.com",
+  },
+];
+export default contacts;
